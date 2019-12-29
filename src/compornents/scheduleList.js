d81429805ea91cfe14c3cf546e3a2510a4a2db79
itanium <is0434ii@ed.ritsumei.ac.jp>
2019-12-29 10:47:35 +0900

    update: 時間の差を求める函数など追加

--- src/compornents/scheduleList.js
+++ src/compornents/scheduleList.js
@@ -3,17 +3,29 @@ import React from 'react';
 class ScheduleList extends React.Component {
   constructor(props) {
     super(props);
-    let sl = [{text:"a",id:"11:00"}];
-    this.state = {showList: sl};
+    let schedules = [{text:"hoge",time:{h:11,m:0}}];
+    this.state = {schedules: schedules};
+    // let startDate = props.now.getDate(); 
   }
 
+  parseTimeStr(time) {
+    let zeroPad = (num) => {return ("00" + num).slice(-2)};
+    return zeroPad(time.h) + ":" + zeroPad(time.m);
+  }
+
+  calcMinDiff(t1,t2){
+    return (((t1.h - t2.h) * 60) + (t1.m - t2.m));
+  }
 
   render() {
-    // console.log(this.props.now);
+    let now = {h: this.props.now.getHours(), m: this.props.now.getMinutes()};
+    console.log(this.parseTimeStr(now));
     return (
       <ul className="nopoint">
-        {this.state.showList.map(item => (
-          <li key={item.id}>{item.id} : {item.text}</li>
+        {this.state.schedules.map(item => (
+          <li key={item.time}>{this.parseTimeStr(item.time)} : {item.text} : 
+            あと{this.calcMinDiff(item.time,now)}分</li>
+          
         ))}
       </ul>
     );
